84bf42a0d3aea9fcfd75cde726251ee47ffe4f03
Jhon Rangel <jhonrangel21@gmail.com>
2025-02-14 16:47:01 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="bg-[url('./assets/images/bg-mobile-ligth.jpg')]">
+            <h1 className="uppercase">Todo</h1>
+            <button className="">Luna</button>
+        </div>
+    );
+};
+
+export default App;
